6be95ea3365e8c34dd7798cb143d51f4f87b7ce7
kdmukai <kdmukai@gmail.com>
2025-04-27 13:32:04 -0500

    remove REPO_NAME env var; simplify html output

--- .github/diff_report/diff_screenshots.py
+++ .github/diff_report/diff_screenshots.py
@@ -4,7 +4,7 @@ differences.
 
 Expected usage in a GitHub Actions workflow; compare `dev` with the `$BRANCH_NAME` in the
 associated PR that triggered the CI run:
-python src/seedsigner/resources/seedsigner-translations/.github/diff_report/diff_screenshots.py ./artifacts/dev ./artifacts/$BRANCH_NAME ./artifacts/diff $REPO_NAME
+python src/seedsigner/resources/seedsigner-translations/.github/diff_report/diff_screenshots.py ./artifacts/dev ./artifacts/$BRANCH_NAME ./artifacts/diff
 """
 import argparse
 import glob
@@ -19,7 +19,6 @@ parser = argparse.ArgumentParser(prog=__name__)
 parser.add_argument("before_dir", type=str, help="Directory containing screenshots before the proposed changes")
 parser.add_argument("after_dir", type=str, help="Directory containing screenshots after the proposed changes")
 parser.add_argument("output_dir", type=str, help="Directory to save the screenshots diff report")
-parser.add_argument("source_repo", type=str, help="Submitter's fork's repo name")
 
 args = parser.parse_args()
 
@@ -93,7 +92,7 @@ for file in list_files_recursively(args.after_dir):
 only_in_before = set(paths_before) - set(paths_after)
 
 html_content = "<h1>Screenshots diff report</h1>"
-html_content += f"""<p>Comparing {before_branch_name} to <a href="https://github.com/SeedSigner/seedsigner-translations/compare/dev...{source_repo}:{after_branch_name}" target="github">{after_branch_name}</a></p>"""
+html_content += f"""<p>Comparing {before_branch_name} to {after_branch_name}</p>"""
 output_dir_before = os.path.join(args.output_dir, "before")
 output_dir_after = os.path.join(args.output_dir, "after")
 os.makedirs(output_dir_before, exist_ok=True)
